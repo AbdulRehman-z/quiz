86733256bd5e7435c0df94a879e3ee955c7bc991
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-06 17:33:22 +0500

    Add start button functionality to StartScreen component

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,15 +1,25 @@
+import { QuestionAction } from "../App";
+
 type StartScreenProps = {
   count: number;
+  dispatch: React.Dispatch<QuestionAction>;
+  questions: unknown[];
 };
 
-function StartScreen({ count }: StartScreenProps) {
+function StartScreen({ count, dispatch, questions }: StartScreenProps) {
+  function handleClick() {
+    dispatch({ type: "start", payload: questions });
+  }
+
   return (
     <div className="flex flex-col items-center gap-3">
       <h1 className="text-5xl font-bold">Welcome to React Wiz!</h1>
       <p className="text-3xl font-semibold">
         {count} Quizez To test your knowledge in React
       </p>
-      <button className="btn mt-10">Lets Begin...</button>
+      <button className="btn mt-10" onClick={handleClick}>
+        Lets Begin...
+      </button>
     </div>
   );
 }
